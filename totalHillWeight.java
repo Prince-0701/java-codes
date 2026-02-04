7e350f1e2d1a0ebb506f88cb20ee300334c10526
Prince-0701 <prince892289@gmail.com>
2026-02-04 20:40:30 +0530

    Add files via upload

--- totalHillWeight.java
+++ totalHillWeight.java
@@ -3,8 +3,8 @@ import java.util.*;
 class UserMainCode{
 	public int totalHillWeight(int input1,int input2,int input3){
 		int weight=0;
-        for(int i=0;i<input1;i++){
-            for(int j=0;j<(i+1);j++){
+        for(int i=0;i<input1;i++){ //rows
+            for(int j=0;j<(i+1);j++){ //col
                 weight+=input2;
             }
             input2+=input3;
